6e82e2fd20cac191679585404a8ff2df3369cb80
maricamola <maricamola98@libero.it>
2023-03-27 18:08:49 +0200

    test snack(5)

---
+++ JSnack 5/script.js
@@ -0,0 +1,13 @@
+let array = []
+
+for(let i = 0 ; i < 6; i++){
+  let number = prompt('Inserisci un numero:');
+  
+  if(number % 2 !==0);
+  array.push(number);{
+  console.log(array)
+  } 
+}
+
+let outputStr = document.getElementById('output') ;
+output.innerHTML= `Numeri dispari: + ${array.push(number)}`;
